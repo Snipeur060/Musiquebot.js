2c1b7425963a3774a56c388c3663c4e55ca8f1a7
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2021-05-06 15:29:24 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const dbd = require("aoi.js")
+
+const bot = new Aoijs.Bot({
+token: "here", 
+prefix: "here" 
+})
+
+
+bot.onMessage()
+
+
+bot.status({
+  text: "Thanks Snipeur060 💻снайпер060⌨#8273",
+  type: "PLAYING",
+  status: "dnd",
+  time: 12
+})
+
+bot.loadCommands('./command/')
