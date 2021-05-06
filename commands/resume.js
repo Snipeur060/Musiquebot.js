5dd1c012c93cbaca40a47e3a037fbd78e9a87d78
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2021-05-06 15:32:43 +0200

    Create resume.js

---
+++ commands/resume.js
@@ -0,0 +1,11 @@
+module.exports = ({
+ name: "resume",
+ code: `
+$onlyIf[$voiceID[$authorID]!=;Vous êtes dans aucun salon]
+$description[La musique à bien été reprise là ou vous l'aviez arrété]
+$color[#FF00FF]
+ $resumeSong
+ $footer[Thanks 💻снайпер060⌨#8273]
+ 
+ `
+ });
